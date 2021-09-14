a3089f87a000e3e81475fe5da5b5590fc411fc8c
android156 <n_degtyarev@mail.ru>
2021-09-14 18:43:20 +0300

    Вроде работает. Проверка на пустой ввод добавлена.

--- app/src/main/java/home/android1/lesson1/MainActivity.java
+++ app/src/main/java/home/android1/lesson1/MainActivity.java
@@ -6,14 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView result;
     private EditText temperature;
     private Button button;
     private RadioGroup radioGroup;
+    private RadioButton celsius;
+    private RadioButton fahrenheit;
+
+
+    private double convertTemperature(double user_temperature, int radio_button_id) {
+        if (radio_button_id == R.id.celsius_radiobutton)
+            return (user_temperature * 1.8 + 32);
+        return (user_temperature - 32) / 1.8;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +36,17 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         radioGroup = findViewById(R.id.user_choice_radioGroup);
 
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int checked_button_id = radioGroup.getCheckedRadioButtonId();
+                String user_input = temperature.getText().toString();
+                if (!user_input.isEmpty()) {
+                    double user_temperature = Double.parseDouble(temperature.getText().toString());
+                    result.setText(String.format("%.1f", convertTemperature(user_temperature, checked_button_id)));
+                }
+            }
+        });
     }
 }
