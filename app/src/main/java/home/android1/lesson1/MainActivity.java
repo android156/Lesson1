e11364d574ce1e88ac3f16cb23cb59bc983e2e6a
android156 <n_degtyarev@mail.ru>
2021-09-14 16:09:13 +0300

    Делаем конвертор градусов. Добавлены все элементы вьюшки.

--- app/src/main/java/home/android1/lesson1/MainActivity.java
+++ app/src/main/java/home/android1/lesson1/MainActivity.java
@@ -3,12 +3,27 @@ package home.android1.lesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView result;
+    private EditText temperature;
+    private Button button;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result_text_view);
+        temperature = findViewById(R.id.temperature_edittext_view);
+        button = findViewById(R.id.button);
+        radioGroup = findViewById(R.id.user_choice_radioGroup);
+
     }
 }
